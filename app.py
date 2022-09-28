4fe7b5e4a749d354b67ac0ed1d0f431370a80fb5
OmThakkar <omthakkar2021.comp@mmcoe.edu.in>
2022-09-28 22:44:04 +0530

    Packages added for the model and Major Style changes

--- app.py
+++ app.py
@@ -7,17 +7,56 @@ app=Flask(__name__)
 
 model=pickle.load(open("random.pkl", "rb"))
 
-@app.route("/n")
+@app.route("/")
 def Home():
-    return render_template(". html")
+    return render_template("medical_history.html")
+
+def dataConvertor (features):
+    finaldata= []
+    for feature in features:
+        if feature[0] == 'name':
+            finaldata.append(feature[1])
+        
+        else:
+            finaldata.append(int(feature[1]))
+    return finaldata
 
 @app.route("/predict",methods=['POST'])
 def predict():
-    float_features = [float (x) for x in request.form.values()]
-    features=[np.array(float_features)]
-    prediction=model.prediction(features)
+    # features =dataConvertor(request.form)
+    # print(features)
+    # float_features = [x for x in request.form.values()]
 
-    return render_template(".html",prediction_text=" jlj".format(prediction))
+    nm = request.form['name'] #name
+    em = request.form['email'] #email
+    #rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr
+    ag = request.form['age'] #age
+    gn = request.form['gender'] #gender
+    cp = request.form['chestpain'] #cp
+    bp = request.form['bloodpressure'] #bp
+    sc = request.form['sech'] #sech
+    fbs = request.form['fbs'] #fbs
+    ecg = request.form['ecg'] #fbs
+    hra = request.form['hra'] #hra
+    eia = request.form['eia']
+    deprst=request.form['deprst']
+    spe = request.form['slope']
+    mv = request.form['vessels']
+    thal = request.form['thal']
+
+    qop = np.array([[ag, gn, cp, bp, sc, fbs, ecg,hra, eia, deprst, spe, mv, thal]])
+    
+    # features=[np.array(float_features)]
+    prrr = model.predict(qop)
+    print(prrr)
+    print(type(prrr[0]))
+    a = "default"
+    if prrr[0] == 1:
+        a = "You might have heart disease"
+    elif prrr[0] == 0:
+        a = "You don't have a Heart disease"
+        
+    return render_template("temp.html",prediction =a)
 
 if __name__ == "__main__":
     app.run(debug=True)
